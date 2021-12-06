89672f17d63ef35e266ef266f1b445b0560f7a3f
Gabriel <gabrielpbenedicto@gmail.com>
2021-12-06 20:20:50 -0300

    Corrigido requisito 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,47 @@
 import React, { useContext } from 'react';
 import Context from '../services/context';
 
+const verifyFilter = (planet, filterByNumericValues, filterByName) => {
+  const { column, comparison, value } = filterByNumericValues;
+  if (filterByName.length !== 0) return planet.name.includes(filterByName);
+
+  if (comparison === 'igual a') {
+    return Number(planet[column]) === Number(value);
+  }
+  if (comparison === 'menor que') {
+    return Number(planet[column]) < Number(value);
+  }
+  if (comparison === 'maior que') {
+    return Number(planet[column]) > Number(value);
+  }
+  if (planet[column] === 'unknown') return false;
+  return true;
+};
+
 function Table() {
   const { data, filterByName, filterByNumericValues, hasFilter } = useContext(Context);
+
+  const renderFilterTable = () => (
+    data.filter((planet) => (verifyFilter(planet,
+      filterByNumericValues[0], filterByName)))
+      .map((planet) => (
+        <tr key={ planet.name }>
+          <td>{ planet.name }</td>
+          <td>{ planet.rotation_period }</td>
+          <td>{ planet.orbital_period }</td>
+          <td>{ planet.diameter }</td>
+          <td>{ planet.climate }</td>
+          <td>{ planet.gravity }</td>
+          <td>{ planet.terrain }</td>
+          <td>{ planet.surface_water }</td>
+          <td>{ planet.population }</td>
+          <td>{ planet.films }</td>
+          <td>{ planet.created }</td>
+          <td>{ planet.edited }</td>
+          <td>{ planet.url }</td>
+        </tr>
+      ))
+  );
 
   return (
     <table>
@@ -40,35 +79,7 @@ function Table() {
           </tr>
         ))
       )
-        : (
-          data.filter((planet) => {
-            const { column, comparison, value } = filterByNumericValues[0];
-            if (comparison === 'igual a') return Number(planet[column]) === Number(value);
-            if (comparison === 'menor que') return Number(planet[column]) < Number(value);
-            if (comparison === 'maior que') return Number(planet[column]) > Number(value);
-            return true;
-          })
-            .filter(({ name }) => {
-              if (filterByName.length !== 0) return name.includes(filterByName);
-              return true;
-            }).map((planet) => (
-              <tr key={ planet.name }>
-                <td>{ planet.name }</td>
-                <td>{ planet.rotation_period }</td>
-                <td>{ planet.orbital_period }</td>
-                <td>{ planet.diameter }</td>
-                <td>{ planet.climate }</td>
-                <td>{ planet.gravity }</td>
-                <td>{ planet.terrain }</td>
-                <td>{ planet.surface_water }</td>
-                <td>{ planet.population }</td>
-                <td>{ planet.films }</td>
-                <td>{ planet.created }</td>
-                <td>{ planet.edited }</td>
-                <td>{ planet.url }</td>
-              </tr>
-            ))
-        )}
+        : renderFilterTable()}
     </table>
   );
 }
